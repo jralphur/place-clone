21b1f8e55cda563aa62cc1c7cd0faf040fe3c964
Joshua Reno <jralphur@gmail.com>
2022-04-11 17:33:01 -0700

    fit vscode/vetur warnings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,12 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  globals: {
+    defineProps: "readonly",
+    defineEmits: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly",
+  },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
